b507e48051aefea2bd9f39d11acb34aeaf540e1d
Harshit Singh <77427143+harshiitsingh@users.noreply.github.com>
2022-08-16 19:56:33 +0530

    first commit

--- 03_app.py
+++
@@ -1,8 +0,0 @@
-# Understanding Jinja2 Template Engine In Flask Web Framework
-
-# JINJA2 TEMPLATE ENGINE
-'''
-{%...%} --> to use condtions, loops statement in my html code
-{{   }} --> expressions to print output
-{#....#}--> this is for comments
-'''
